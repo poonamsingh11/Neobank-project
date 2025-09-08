47e462f2ab76467ab3c3bc154db58fcc03f4732f
Poonam singh rajput <Poonamsinghrajput534@gmail.com>
2025-09-08 18:18:26 +0530

    Initial commit - Neobank Project structure

---
+++ src/main/java/com/example/Neobank/Project/NeobankProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Neobank.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeobankProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeobankProjectApplication.class, args);
+	}
+
+}
