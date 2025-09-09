c3b9bb981012f2c821cde4ae9046732a0642167c
vikagour123 <vikagour798@gmail.com>
2025-09-09 16:32:50 +0530

    changes kar rha hu add kar dena

--- src/main/java/com/example/Neobank/Project/NeobankProjectApplication.java
+++ src/main/java/com/example/Neobank/Project/NeobankProjectApplication.java
@@ -8,7 +8,7 @@ public class NeobankProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NeobankProjectApplication.class, args);
-		System.out.println("Changes Applied by vikas gour");
+		System.out.println("New changes made by me ");
 	}
 
 }
