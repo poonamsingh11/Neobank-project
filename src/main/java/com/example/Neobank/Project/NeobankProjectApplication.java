df16e61f9952d4ba04d83cbfebe6a46e2084bc21
Amit Rajput <rajputamit9617@gmail.com>
2025-09-10 11:13:15 +0530

    Add NeoLogin feature

--- src/main/java/com/example/Neobank/Project/NeobankProjectApplication.java
+++ src/main/java/com/example/Neobank/Project/NeobankProjectApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NeobankProjectApplication {
-
-	public static void main(String[] args) {
+public class NeobankProjectApplication 
+{
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(NeobankProjectApplication.class, args);
-		
+		System.out.println("ram..");
 		
 	}
 
