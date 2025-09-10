15e1ec6bc11e645500a7537dd08d9fa53698d8a2
vikagour123 <vikagour798@gmail.com>
2025-09-10 10:36:34 +0530

    new changes

--- src/main/java/com/example/Neobank/Project/NeobankProjectApplication.java
+++ src/main/java/com/example/Neobank/Project/NeobankProjectApplication.java
@@ -9,7 +9,7 @@ public class NeobankProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NeobankProjectApplication.class, args);
 		
-		
+		System.out.println("jay shree ram ");
 	}
 
 }
