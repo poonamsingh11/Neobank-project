af716c0b6234b8ce855de867c1141c81224c417d
Poonam singh rajput <Poonamsinghrajput534@gmail.com>
2025-09-18 13:06:24 +0530

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Components
 import Navbar from "./components/Navbar";
+
+// Pages
 import DashBoard from "./pages/DashBoard";
-const App=()=>{
-  return(
-    <>
+import DepositsPage from "./pages/Deposits/DepositsPage";
+import FixedDepositForm from "./pages/Deposits/FixedDepositForm";
+import RDPage from "./pages/Deposits/RDPage";
+import FdCalculator from "./pages/Deposits/FdCalculator";
+import TaxSaverFD from "./pages/Deposits/TaxSaverFD";
+
+
+
+
+const App = () => {
+  return (
     <Router>
-      <Navbar/>
+      {/* ✅ Navbar हर page पर common */}
+      <Navbar />
       <Routes>
-        <Route  path="/" element={<DashBoard/>}/>
+        {/* Home/Dashboard */}
+        <Route path="/" element={<DashBoard />} />
+
+        {/* Deposits Main Page */}
+        <Route path="/deposit" element={<DepositsPage />} />
+
+        {/* Fixed Deposit */}
+        <Route path="/fd-calculator" element={<FdCalculator />} />
+        <Route path="/fixed-deposit" element={<FixedDepositForm />} />
+
+        {/* Recurring Deposit */}
+        <Route path="/recurring-deposit" element={<RDPage />} />
+        <Route path="/tax-saver-fd11" element={<TaxSaverFD />} />
       </Routes>
     </Router>
-    </>
-  )
-}
+  );
+};
+
 export default App;
